5a1316088ca0ebb4ebc1e7b240e58e3d3a4d784b
Marcin Hyla <marcinhyla123@gmail.com>
2026-04-16 14:42:37 +0200

    ingest all ff files

--- src/ingest.py
+++ src/ingest.py
@@ -4,6 +4,14 @@ from pathlib import Path
 
 # data ingest
 base_dir = Path(__file__).resolve().parent.parent
-filepath = base_dir / 'data' / 'pigeonflocks_trajectories' / 'ff1' / 'ff1_A.txt'
-data = pd.read_csv(filepath, sep='\t', header=None, comment='#')
-print(data.head())
+ff_files = sorted((base_dir / 'data' / 'pigeonflocks_trajectories').glob('ff*/ff*.txt'))
+frames = []
+for ff_file in ff_files:
+    df = pd.read_csv(ff_file, sep='\t', header=None, comment='#')
+    df["flight"] = ff_file.parent.name
+    df['bird'] = ff_file.stem.split('_')[-1]
+    df["source"] = str(ff_file)
+    frames.append(df)
+
+all_ff = pd.concat(frames, ignore_index=True)
+print(all_ff.head())
